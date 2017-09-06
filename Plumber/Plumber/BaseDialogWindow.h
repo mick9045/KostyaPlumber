0d980352ba603ddb50cc769bebba6558e28ea4a2
mick9045 <mick9045@gmail.com>
2017-09-06 19:23:47 +0300

    added main

---
+++ Plumber/Plumber/BaseDialogWindow.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <windows.h>
+#include <CommCtrl.h>
+#include <string>
+//#include "resource.h"
+
+#ifndef UNICODE  
+typedef std::string tString;
+#define to_tstring std::to_string
+#else
+typedef std::wstring tString;
+#define to_tstring std::to_wstring
+#endif
+
+
+namespace CourseWork_WinApi
+{
+	class BaseDialogWindow
+	{
+	public:
+		virtual  ~BaseDialogWindow();
+		BaseDialogWindow(INT nResource);
+		BOOL IsCreated();
+		HWND Create(HWND hwndParent = NULL, HINSTANCE hInstance = GetModuleHandle(NULL));
+		INT_PTR CreateModal(HWND hwndParent = NULL, HINSTANCE hInstance = GetModuleHandle(NULL));
+		HWND GetWindowHandle();
+		INT GetResourceID();
+		HWND hWindow();
+		SIZE GetClientSize();
+		SIZE GetSize();
+		RECT GetRect();
+		POINT GetLocalPosition();
+		static tString GetText(HWND hWnd);
+		static POINT GetLocalPosition(HWND hWnd);
+		static SIZE GetClientSize(HWND hWnd);
+		static SIZE GetSize(HWND hWnd);
+		static INT GetMenuItemPosById(HMENU hMenu, INT id);
+		/*~~~~~~~~~~~~~~ methods~~~~~~~~~~~~~~~~~~*/
+		void Close();
+	protected:
+		HWND _hwnd;
+		INT _nResource;
+		INT _nResult;
+		/*~~~~~~~~~~~~~~ methods~~~~~~~~~~~~~~~~~~*/
+		BOOL isModal();
+		inline static BaseDialogWindow * GetWindowObjectFromHandle(HWND hWnd);
+		virtual INT_PTR DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) = 0;
+	private:
+		BOOL _bModal;
+		//prohobit coping
+		BaseDialogWindow(const BaseDialogWindow &);
+		static INT_PTR CALLBACK MessageRoute(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); // routes messages from all windows
+	};
+
+}
--- Plumber/Plumber/main.cpp
+++ Plumber/Plumber/main.cpp
@@ -1,6 +1,38 @@
 #include <Windows.h>
+#include <tchar.h>
 
-int main()
+
+#include <CommCtrl.h>
+
+#pragma comment(lib, "comctl32")
+
+void inline EnableCommonControls();
+
+INT WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, INT nCmdShow)
 {
+	EnableCommonControls();
+	MSG msg;
+
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
+		/*
+		if (!IsDialogMessage(hMainWindow, &msg))
+		{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		}
+		*/
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return NULL;
+}
+
+
+void inline EnableCommonControls()
+{
+	INITCOMMONCONTROLSEX icc = { sizeof(INITCOMMONCONTROLSEX), ICC_WIN95_CLASSES };
+	InitCommonControlsEx(&icc);
+	LoadLibrary(TEXT("RICHED20.DLL"));
 
 }
